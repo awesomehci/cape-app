df087e3e7a55a0eeb61727af4dfe499ea4ebfbf3
Sarah <sarahms@ufl.edu>
2017-03-01 10:56:55 -0500

    Added dummy data, started layout

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//Renders original "index" page
+//app.get('/', function(request, response) {
+//  response.render('pages/index');
+//});
+
+//Renders new "view" page
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/view');
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
